0ba8351464552a86c59b7f6b7fa87d693787d176
Matthew Drummond <mattatatter@gmail.com>
2021-10-16 19:26:17 -0500

    start working on tables

--- CLI/Table.py
+++ CLI/Table.py
@@ -2,32 +2,44 @@ from Database import Database
 from Constants import Queries
 from Constants import TableNames
 
-class BaseTable:
-    def __init__(self, name, rows, db):
-        self.name = name
-        self.rows = rows
-        self.__db = db
-
-    def FindIndex(self, id):
-        if len(self.rows) == 0:
-            self.rows = self.Select()
-        i = 0
-        for row in rows:
-            if row.id == id:
-                break
-            else:
-                i += 1
-        return i
+#TODO: create types for each table row
+# table types
+# class TableRow:
+#     def __init__(self, *data):
+#         self.data = data
+#         pass
 
 
-    def Insert(self, row):
-        self.rows.append(row)
+# class Table:
+#     def __init__(self, name: str, db: Database):
+#         self.name: str = name
+#         self.__db: Database = db
 
-    def Select(self):
-        return db.SelectQuery(Queries['Select'] + f" * FROM {TableNames[self.name]}")
+#     def FindIndex(self, id) -> int:
+#         rows = self.Select(self)
+#         i = 0
+#         for row in rows:
+#             if row.id == id:
+#                 break
+#             else:
+#                 i += 1
+#         return i
 
-    def Update(self, id, newRow):
-        row[self.FindIndex(id)] = newRow
 
-    def Delete(self, id):
-        rows.remove(self.FindIndex(id))
+#     def Insert(self, obj: TableRow):
+#         vals = ""
+#         for d in obj.data:
+#             vals += d
+
+#         self.__db.ChangeQuery(Queries['Insert' + f" INTO {self.name} VALUES({vals})"])
+
+
+#     def Select(self):
+#         return self.__db.SelectQuery(Queries['Select'] + f" * FROM {TableNames[self.name]}")
+
+#     def Update(self, id, newRow):
+#         return self.__db.ChangeQuery(Queries['Update'] + f" TABLE {TableNames[self.name]} ")
+#         row[self.FindIndex(id)] = newRow
+
+#     def Delete(self, id):
+#         rows.remove(self.FindIndex(id))
