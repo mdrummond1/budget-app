e9538458b947f8727bc84c3b3c908517779342fd
Matthew Drummond <mattatatter@gmail.com>
2021-11-08 20:21:20 -0600

    add vendor/vendorType classes

---
+++ CLI/Tables/Vendor.py
@@ -0,0 +1,11 @@
+import Database
+
+class Vendor:
+    def __init__(self, row: list):
+        self.vendor_id = row[0]
+        self.vendor_type = row[1]
+        self.name = row[2]
+        self.web_address = row[3]
+
+    def def __str__(self) -> str:
+        return f"id: {self.vendor_id} vendor_type: {self.vendor_type} name: {self.name} web address: {self.web_address}"
---
+++ CLI/Tables/VendorType.py
@@ -0,0 +1,7 @@
+class VendorType:
+    def __init__(self, row: list):
+        self.vendor_type_id = row[0]
+        self.vendor_type = row[1]
+
+    def __str__(self):sldkfj;alskdjf;kajf
+        return f"id: {self.vendor_type_id} vendor category:{self.vendor_type}"
