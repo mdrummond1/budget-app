09be2ddf3218cb849d0f3514f188fca0ef7a9d7d
Matthew Drummond <mattatatter@gmail.com>
2021-11-14 22:33:15 -0600

    remove erroneous object indexing

--- CLI/MenuFunctions.py
+++ CLI/MenuFunctions.py
@@ -7,21 +7,21 @@ def format_menu(s):
 
 def print_obj_list(l: list):
     for i, el in enumerate(l, start=1):
-        print(f"{i}. {el[0]}")
+        print(f"{i}. {el}")
 
 def select_obj_from_list(l: list, obj_type):
     print_obj_list(l)
     sel = input(f"Select {obj_type} from list:")
     try:
         sel = int(sel)
-        if sel < len(l):
+        if sel < (len(l) + 1):
             return l[sel-1]
         else:
             return -1
     except:
         print("invalid entry. Cancelling...")
 
-def get_datetime():
+def get_datetime() -> datetime:
     year = input("Enter year: ")
     month = input("Enter month:")
     day = input("Enter day: ")
